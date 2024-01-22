1256809fa9e175ed225362eb296ab061560b90ab
leogianluca <leogianluca.costa@gmail.com>
2024-01-22 15:57:22 -0300

    about

--- src/data/features-data.js
+++ src/data/features-data.js
@@ -5,25 +5,30 @@ import {
   GlobeAmericasIcon,
 } from "@heroicons/react/24/solid";
 
+[15:51, 22/01/2024] +55 27 99652-8443: serviços de terraplanagem
+[15:51, 22/01/2024] +55 27 99652-8443: conservação de rodovias
+[15:52, 22/01/2024] +55 27 99652-8443: serviços de poda e jardinagem
+[15:52, 22/01/2024] +55 27 99652-8443: oficina mecanica
+
 export const featuresData = [
   {
     color: "gray",
-    title: "CREDIBILIDADE E TRANSPARÊNCIA CORPORATIVA",
+    title: "SERVIÇOS DE TERRAPLANAGEM",
     icon: IdentificationIcon,
   },
   {
     color: "gray",
-    title: "COMPROMETIMENTO COM A MISSÃO",
+    title: "CONSERVAÇÃO DE RODOVIAS",
     icon: BriefcaseIcon,
   },
   {
     color: "gray",
-    title: "RESPONSABILIDADE SOCIOAMBIENTAL",
+    title: "SERVIÇOS DE PODA E JARDINAGEM",
     icon: GlobeAmericasIcon,
   },
   {
     color: "gray",
-    title: "VALORIZAÇÃO DAS PESSOAS E DA VIDA",
+    title: "OFICINA MECÂNICA",
     icon: UserGroupIcon,
   },
 ];
