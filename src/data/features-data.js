4771fc87e86c3e525db4e6d3b419b41255b6baa3
leogianluca <leogianluca.costa@gmail.com>
2024-01-22 16:20:42 -0300

    about

--- src/data/features-data.js
+++ src/data/features-data.js
@@ -1,14 +1,6 @@
 import {
   IdentificationIcon,
-  UserGroupIcon,
-  BriefcaseIcon,
-  GlobeAmericasIcon,
 } from "@heroicons/react/24/solid";
-
-[15:51, 22/01/2024] +55 27 99652-8443: serviços de terraplanagem
-[15:51, 22/01/2024] +55 27 99652-8443: conservação de rodovias
-[15:52, 22/01/2024] +55 27 99652-8443: serviços de poda e jardinagem
-[15:52, 22/01/2024] +55 27 99652-8443: oficina mecanica
 
 export const featuresData = [
   {
@@ -19,17 +11,17 @@ export const featuresData = [
   {
     color: "gray",
     title: "CONSERVAÇÃO DE RODOVIAS",
-    icon: BriefcaseIcon,
+    icon: IdentificationIcon,
   },
   {
     color: "gray",
     title: "SERVIÇOS DE PODA E JARDINAGEM",
-    icon: GlobeAmericasIcon,
+    icon: IdentificationIcon,
   },
   {
     color: "gray",
     title: "OFICINA MECÂNICA",
-    icon: UserGroupIcon,
+    icon: IdentificationIcon,
   },
 ];
 
